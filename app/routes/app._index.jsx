9d4d4e36f94d8b45f8b033b1b3f1481c8fcbd33f
Sifat Ahmed <programmer.sifatahmed@gmail.com>
2026-05-21 15:38:31 +0600

    fix(PageHeader): Page header spacing fixed

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -14,7 +14,7 @@ export default function Index() {
   // End----Default CSR loading state checking for navigation
   return (
     <s-page>
-      <s-stack direction='inline' gap='base' justifyContent='space-between' alignItems='center'>
+      <s-stack direction='inline' gap='base' justifyContent='space-between' alignItems='center' paddingBlockEnd='base'>
         <Text as='h2'>Welcome to Qorix review 👋</Text>
         <s-grid gridTemplateColumns='auto auto' gap='base'>
           <s-button variant='secondary' icon='plus'>
