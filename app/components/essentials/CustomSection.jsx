ec667e5fdf1aa4534518896c45ec877fcb720dc7
Sifat Ahmed <programmer.sifatahmed@gmail.com>
2026-06-08 16:53:45 +0600

    chor(TrustBar-Edit): Added TrustBar editor template page

--- app/components/essentials/CustomSection.jsx
+++ app/components/essentials/CustomSection.jsx
@@ -1,19 +1,64 @@
+const DEFAULT_BORDER_STYLES = {
+    borderWidth: "1px",
+    borderStyle: "solid",
+    borderColor: "#e4e4e4ff",
+};
+
 export default function CustomSection({
     children,
     padding = "base",
     margin = "0px",
     background = "transparent",
-    border = "1px solid #e4e4e4ff",
+    border,
+    borderTop,
+    borderRight,
+    borderBottom,
+    borderLeft,
+    borderBlock,
+    borderBlockStart,
+    borderBlockEnd,
+    borderInline,
+    borderInlineStart,
+    borderInlineEnd,
+    borderColor,
+    borderStyle,
+    borderWidth,
     boxShadow = "0px 1px 3px 0px #e4e4e493",
     borderRadius = "12px",
+    borderTopLeftRadius,
+    borderTopRightRadius,
+    borderBottomRightRadius,
+    borderBottomLeftRadius,
     overflow = "inherit",
     aspectRatio = "inherit",
     display = "block",
     alignItems = "start",
-    justifyContent = "start"
+    justifyContent = "start",
 }) {
+    const borderStyles = {
+        ...DEFAULT_BORDER_STYLES,
+        ...(border !== undefined ? { border } : {}),
+        ...(borderTop !== undefined ? { borderTop } : {}),
+        ...(borderRight !== undefined ? { borderRight } : {}),
+        ...(borderBottom !== undefined ? { borderBottom } : {}),
+        ...(borderLeft !== undefined ? { borderLeft } : {}),
+        ...(borderBlock !== undefined ? { borderBlock } : {}),
+        ...(borderBlockStart !== undefined ? { borderBlockStart } : {}),
+        ...(borderBlockEnd !== undefined ? { borderBlockEnd } : {}),
+        ...(borderInline !== undefined ? { borderInline } : {}),
+        ...(borderInlineStart !== undefined ? { borderInlineStart } : {}),
+        ...(borderInlineEnd !== undefined ? { borderInlineEnd } : {}),
+        ...(borderColor !== undefined ? { borderColor } : {}),
+        ...(borderStyle !== undefined ? { borderStyle } : {}),
+        ...(borderWidth !== undefined ? { borderWidth } : {}),
+        ...(borderTopLeftRadius !== undefined ? { borderTopLeftRadius } : {}),
+        ...(borderTopRightRadius !== undefined ? { borderTopRightRadius } : {}),
+        ...(borderBottomRightRadius !== undefined ? { borderBottomRightRadius } : {}),
+        ...(borderBottomLeftRadius !== undefined ? { borderBottomLeftRadius } : {}),
+    };
+
     return (
-        <div style={{ borderRadius: borderRadius, border: border, boxShadow: boxShadow, margin: margin, background: background, overflow: overflow, aspectRatio: aspectRatio, display: display, alignItems: alignItems, justifyContent: justifyContent }}>
+        <div style={{ borderRadius: borderRadius, ...borderStyles, boxShadow: boxShadow, margin: margin, background: background, overflow: overflow, aspectRatio: aspectRatio, display: display, alignItems: alignItems, justifyContent: justifyContent }}>
             <s-stack padding={padding}>
                 {children}
             </s-stack>
--- app/routes/app.widgets/components/WidgetItem.jsx
+++ app/routes/app.widgets/components/WidgetItem.jsx
@@ -1,13 +1,26 @@
+/* eslint-disable react/prop-types */
+
 import CustomSection from "../../../components/essentials/CustomSection";
+import { useAppWindowClose } from "../utils/useAppWindowClose";
 
 export default function WidgetItem({ widget, status = "NOT_INSTALLED" }) {
+    const appWindowId = `edit-window${widget?.id}`;
+
+    useAppWindowClose({
+        appWindowId,
+        widgetId: widget?.id,
+        onClose: () => {
+            console.log("App window hidden:", widget?.id);
+        },
+    });
+
     return (
         <>
-            <s-app-window id={`edit-window${widget?.id}`} src={widget?.editUrl}></s-app-window>
+            <s-app-window id={appWindowId} src={widget?.editUrl}></s-app-window>
             <CustomSection background="#fff" border="none" boxShadow="none" padding="none" overflow="hidden">
                 <CustomSection background="#E1D8AF" border="none" boxShadow="none" borderRadius="0" aspectRatio="1/0.83">
                     <CustomSection background="#fff" padding="none" aspectRatio="1/0.8" display="flex" alignItems="center" justifyContent="center" overflow="hidden">
-                        <img src={widget?.previewUrl} />
+                        <img src={widget?.previewUrl} alt={`${widget?.name} preview`} />
                     </CustomSection>
                 </CustomSection>
                 <s-grid gap="small" padding="small base">
@@ -18,7 +31,7 @@ export default function WidgetItem({ widget, status = "NOT_INSTALLED" }) {
                     <s-paragraph color="subdued">{widget?.description}</s-paragraph>
                     <s-stack direction="inline" justifyContent="space-between" alignItems="center">
                         <s-button disabled={status === "INSTALLED"}>{status === "INSTALLED" ? "Embeded" : "Embed now"}</s-button>
-                        <s-button icon="paint-brush-round"  command="--show" commandFor={`edit-window${widget?.id}`}>Customize</s-button>
+                        <s-button icon="paint-brush-round"  command="--show" commandFor={appWindowId}>Customize</s-button>
                     </s-stack>
                 </s-grid>
             </CustomSection>
