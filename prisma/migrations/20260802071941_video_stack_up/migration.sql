292cdc9dd376b8c67dce97989c408efdcd8a3c87
Md Abdur Razzak <razzakbdcalling24@gmail.com>
2026-08-04 14:44:46 +0600

    refactor: remove deprecated utility functions and unused configuration files

--- prisma/migrations/20260802071941_video_stack_up/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `fiteringMinStart` column on the `VideoStackSettings` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "VideoStackSettings" DROP COLUMN "fiteringMinStart",
-ADD COLUMN     "fiteringMinStart" "FilterMinStars" NOT NULL DEFAULT 'ALL';
